dea56b22b72b89de3644ab5b3daa99af0c043ba0
Jagan Panigrahy <jaganx97@gmail.com>
2024-03-24 04:51:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,6 +7,16 @@ document.getElementsByTagName('body')[0].style.color='white';
 document.getElementsByTagName('button')[0].innerHTML='Day Mode';
 document.getElementsByTagName('button')[0].style.backgroundColor='white';
 document.getElementsByTagName('button')[0].style.color='black';
+document.getElementById('main').style.backgroundImage='url("nightmain.jpg")';
+document.getElementById('submain').style.backgroundColor='rgba(0,0,0,0.5)';
+document.getElementById('supermain').style.backgroundColor='rgba(0,0,0,0.7)';
+document.getElementById('ult').style.backgroundImage='url("night.jpg")';
+for(i=0;i<3;i++)
+{
+document.getElementsByTagName('a')[i].style.backgroundColor='black';
+document.getElementsByTagName('a')[i].style.color='white';
+document.getElementsByTagName('a')[i].style.borderColor='white';
+}
 }
 else
 {
@@ -15,5 +25,15 @@ document.getElementsByTagName('body')[0].style.color='black';
 document.getElementsByTagName('button')[0].innerHTML='Night Mode';
 document.getElementsByTagName('button')[0].style.backgroundColor='black';
 document.getElementsByTagName('button')[0].style.color='white';
+document.getElementById('main').style.backgroundImage='url("daymain.jpg")';
+document.getElementById('submain').style.backgroundColor='rgba(255,255,255,0.5)';
+document.getElementById('supermain').style.backgroundColor='rgba(255,255,255,0.5)';
+document.getElementById('ult').style.backgroundImage='url("day.jpg")';
+for(i=0;i<3;i++)
+{
+document.getElementsByTagName('a')[i].style.backgroundColor='white';
+document.getElementsByTagName('a')[i].style.color='black';
+document.getElementsByTagName('a')[i].style.borderColor='black';
+}
 }
 }
