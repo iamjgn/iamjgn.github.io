46397dfe58ca1d02d37f93bb1667a47d18a35f70
Jagan Panigrahy <jaganx97@gmail.com>
2024-04-03 18:40:57 +0530

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -10,9 +10,7 @@ document.getElementsByTagName('button')[0].style.color='black';
 document.getElementsByTagName('body')[0].style.backgroundImage='url("res/night.jpg")';
 for(i=0;i<3;i++)
 {
-document.getElementsByTagName('a')[i].style.backgroundColor='black';
 document.getElementsByTagName('a')[i].style.color='white';
-document.getElementsByTagName('a')[i].style.borderColor='white';
 }
 }
 else
@@ -25,9 +23,7 @@ document.getElementsByTagName('button')[0].style.color='white';
 document.getElementsByTagName('body')[0].style.backgroundImage='url("res/day.jpg")';
 for(i=0;i<3;i++)
 {
-document.getElementsByTagName('a')[i].style.backgroundColor='white';
 document.getElementsByTagName('a')[i].style.color='black';
-document.getElementsByTagName('a')[i].style.borderColor='black';
 }
 }
 }
