a9bc8cabdcaa2399b668482e4c5362611c6fdd46
Jagan Panigrahy <jaganx97@gmail.com>
2024-04-16 21:40:36 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ function theme()
 {
 if (document.getElementsByTagName('body')[0].style.color=='black')
 	{
-	document.getElementsByTagName('body')[0].style.backgroundColor='#0d0d0d;
+	document.getElementsByTagName('body')[0].style.backgroundColor='#0d0d0d';
 	document.getElementsByTagName('body')[0].style.color='white';
 	document.getElementsByTagName('button')[0].innerHTML='Day Mode';
 	document.getElementsByTagName('button')[0].style.backgroundColor='white';
