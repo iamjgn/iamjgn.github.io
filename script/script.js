01ecd0714e1f9222ccab4c56e93c95749f66660b
Jagan Panigrahy <jaganx97@gmail.com>
2024-04-16 21:27:13 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,12 +2,11 @@ function theme()
 {
 if (document.getElementsByTagName('body')[0].style.backgroundColor=='white')
 	{
-	document.getElementsByTagName('body')[0].style.backgroundColor='black';
+	document.getElementsByTagName('body')[0].style.backgroundColor='#0d0d0d;
 	document.getElementsByTagName('body')[0].style.color='white';
 	document.getElementsByTagName('button')[0].innerHTML='Day Mode';
 	document.getElementsByTagName('button')[0].style.backgroundColor='white';
 	document.getElementsByTagName('button')[0].style.color='black';
-	document.getElementsByTagName('body')[0].style.backgroundImage='url("res/night.jpg")';
 	for(i=0;i<3;i++)
 		{
 		document.getElementsByTagName('a')[i].style.color='black';
@@ -20,12 +19,11 @@ if (document.getElementsByTagName('body')[0].style.backgroundColor=='white')
 	}
 else
 	{
-	document.getElementsByTagName('body')[0].style.backgroundColor='white';
+	document.getElementsByTagName('body')[0].style.backgroundColor='#f5f5f5';
 	document.getElementsByTagName('body')[0].style.color='black';
 	document.getElementsByTagName('button')[0].innerHTML='Night Mode';
 	document.getElementsByTagName('button')[0].style.backgroundColor='black';
 	document.getElementsByTagName('button')[0].style.color='white';
-	document.getElementsByTagName('body')[0].style.backgroundImage='url("res/day.jpg")';
 	for(i=0;i<3;i++)
 		{
 		document.getElementsByTagName('a')[i].style.color='white';
