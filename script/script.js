23f262f4ebf61a107881c1ce4a2c73927b4cc3ce
Jagan Panigrahy <jaganx97@gmail.com>
2024-04-16 20:05:55 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -8,7 +8,7 @@ if (document.getElementsByTagName('body')[0].style.backgroundColor=='white')
 	document.getElementsByTagName('button')[0].style.backgroundColor='white';
 	document.getElementsByTagName('button')[0].style.color='black';
 	document.getElementsByTagName('body')[0].style.backgroundImage='url("res/night.jpg")';
-	for(i=0;i<2;i++)
+	for(i=0;i<3;i++)
 		{
 		document.getElementsByTagName('a')[i].style.color='black';
 		document.getElementsByTagName('a')[i].style.backgroundColor='white';
@@ -26,7 +26,7 @@ else
 	document.getElementsByTagName('button')[0].style.backgroundColor='black';
 	document.getElementsByTagName('button')[0].style.color='white';
 	document.getElementsByTagName('body')[0].style.backgroundImage='url("res/day.jpg")';
-	for(i=0;i<2;i++)
+	for(i=0;i<3;i++)
 		{
 		document.getElementsByTagName('a')[i].style.color='white';
 		document.getElementsByTagName('a')[i].style.backgroundColor='black';
