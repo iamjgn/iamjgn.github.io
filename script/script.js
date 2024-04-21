cfcd9bcb9480feeb8df0948cb77c9dc2e40b1834
Jagan Panigrahy <jaganx97@gmail.com>
2024-04-21 22:22:25 +0530

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,37 +1,65 @@
 function theme()
-{
-if (document.getElementsByTagName('body')[0].style.color=='black')
 	{
-	document.getElementsByTagName('body')[0].style.backgroundColor='#0d0d0d';
-	document.getElementsByTagName('body')[0].style.color='white';
-	document.getElementsByTagName('button')[0].innerHTML='Day Mode';
-	document.getElementsByTagName('button')[0].style.backgroundColor='white';
-	document.getElementsByTagName('button')[0].style.color='black';
-	for(i=0;i<3;i++)
+	if(document.getElementsByTagName('body')[0].style.color=='black')
 		{
-		document.getElementsByTagName('a')[i].style.color='black';
-		document.getElementsByTagName('a')[i].style.backgroundColor='white';
+		document.getElementsByTagName('body')[0].style.backgroundColor='black';
+		document.getElementsByTagName('body')[0].style.color='white';
+		document.getElementsByTagName('button')[0].innerHTML='Day Mode';
+		document.getElementsByTagName('button')[0].style.backgroundColor='white';
+		document.getElementsByTagName('button')[0].style.color='black';
+		for(i=0;i<3;i++)
+			{
+			document.getElementsByTagName('a')[i].style.color='white';
+			document.getElementsByTagName('a')[i].style.borderColor='white';
+			}
+		for(i=0;i<10;i++)
+			{
+			document.getElementById('skills').children[i].style.borderColor='white';
+			}
 		}
-	for(i=0;i<10;i++)
+	else
 		{
-		document.getElementById('skills').children[i].style.borderColor='white';
+		document.getElementsByTagName('body')[0].style.backgroundColor='white';
+		document.getElementsByTagName('body')[0].style.color='black';
+		document.getElementsByTagName('button')[0].innerHTML='Night Mode';
+		document.getElementsByTagName('button')[0].style.backgroundColor='black';
+		document.getElementsByTagName('button')[0].style.color='white';
+		for(i=0;i<3;i++)
+			{
+			document.getElementsByTagName('a')[i].style.color='black';
+			document.getElementsByTagName('a')[i].style.borderColor='black';
+			}
+		for(i=0;i<10;i++)
+			{
+			document.getElementById('skills').children[i].style.borderColor='black';
+			}
 		}
 	}
-else
+
+function btn(x)
 	{
-	document.getElementsByTagName('body')[0].style.backgroundColor='#f5f5f5';
-	document.getElementsByTagName('body')[0].style.color='black';
-	document.getElementsByTagName('button')[0].innerHTML='Night Mode';
-	document.getElementsByTagName('button')[0].style.backgroundColor='black';
-	document.getElementsByTagName('button')[0].style.color='white';
-	for(i=0;i<3;i++)
+	if (x.style.color=='black')
 		{
-		document.getElementsByTagName('a')[i].style.color='white';
-		document.getElementsByTagName('a')[i].style.backgroundColor='black';
+		x.style.color='white';
+		x.style.backgroundColor='black';
 		}
-	for(i=0;i<10;i++)
+	else
 		{
-		document.getElementById('skills').children[i].style.borderColor='black';
+		x.style.color='black';
+		x.style.backgroundColor='white';
 		}
 	}
-}
+
+function ntb(x)
+	{
+	if (x.style.color=='white')
+		{
+		x.style.color='black';
+		x.style.backgroundColor='transparent';
+		}
+	else
+		{
+		x.style.color='white';
+		x.style.backgroundColor='transparent';
+		}
+	}
